e8a4660a90508e6cac3f57af3e7007c1a32869db
Yevhenii Khyzhniak <keildra258@gmail.com>
2023-03-07 01:17:07 +0200

    added script for swipe lower frame

--- _source/js/app.js
+++ _source/js/app.js
@@ -5,3 +5,24 @@ document.querySelector('.js-menu-btn').onclick = function () {
 };
 
 
+let btns = document.querySelectorAll('.links__btn')
+btns.forEach(function (element) {
+
+    element.onclick = swipe;
+})
+
+function swipe(event) {
+    event.preventDefault();
+    let data = this.getAttribute('data');
+
+    btns.forEach(function (element) {
+        element.classList.remove('btn-blue');
+    })
+    document.querySelector(`.links__btn[data="${data}"]`).classList.add('btn-blue');
+
+    let block = document.querySelectorAll('.links__block')
+    block.forEach(function (elem) {
+        elem.classList.remove('visible');
+    })
+    document.querySelector(`.links__block[data="${data}"]`).classList.add('visible');
+}
